b53f2987842d9609a219ffff4a75b6c9b99fd91f
bigbaby321 <phamtaiytb@gmail.com>
2025-04-09 16:13:33 +0700

    update bot

--- bot.py
+++ bot.py
@@ -11,7 +11,6 @@ def main():
     updater = Updater(TOKEN, use_context=True)
     dp = updater.dispatcher
     dp.add_handler(CommandHandler("start", start))
-
     updater.start_polling()
     updater.idle()
 
